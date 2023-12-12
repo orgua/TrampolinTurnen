96982d878820e78f9fc5ad7cc325dcd2360f6359
Ingmar Splitt <orgua@users.noreply.github.com>
2023-12-12 13:09:17 +0100

    only update website on release

--- docs/conf.py
+++ docs/conf.py
@@ -11,7 +11,7 @@ project = "Trampolinturnen"
 project_full = "Einführung ins Trampolinturnen"
 copyright = "2022-2024, Ingmar Splitt"
 author = "Ingmar Splitt, Jan Voigt"
-release = "0.8.8"
+release = "0.8.9"
 builder = "html latexpdf"
 
 # -- General configuration ---------------------------------------------------
